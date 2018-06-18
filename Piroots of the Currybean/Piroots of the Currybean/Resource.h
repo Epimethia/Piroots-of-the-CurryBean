c7215977b830b06e949dbd3316637b3f3b9f4d38
Epimethia <jaden.links@gmail.com>
2018-06-18 14:16:25 +1200

    Started Networking

--- Piroots of the Currybean/Piroots of the Currybean/Resource.h
+++ Piroots of the Currybean/Piroots of the Currybean/Resource.h
@@ -29,11 +29,45 @@
 #include <memory>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
+#include <strstream>
+
+
+//Network Programming
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
 
 namespace UTILS {
 	const int WindowWidth = 750;
 	const int WindowHeight = 750;
 }
+
+#define VALIDATE(a) if (!a) return (false)
+
+namespace {
+	std::string ToString(sockaddr_in _sockAddress)
+	{
+		//INET_ADDRSTRLEN - maximum length for IPv4 addresses
+		char _pcAddress[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &_sockAddress.sin_addr, _pcAddress, INET_ADDRSTRLEN);
+
+		std::string _strAddress = _pcAddress;
+		std::string _strPort = std::to_string(ntohs(_sockAddress.sin_port));
+		std::string _strAddressPort = _strAddress + ':' + _strPort;
+
+		return _strAddressPort;
+	}
+}
+
+template<typename T>
+std::string ToString(const T& _value)
+{
+	std::strstream theStream;
+	theStream << _value << std::ends;
+	return (theStream.str());
+}
+
 
 #pragma region SHADER MACROS
 //Entity Shader
---
+++ Piroots of the Currybean/Piroots of the Currybean/Socket.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Resource.h"
+
+class CSocket {
+public:
+	CSocket();
+	~CSocket();
+
+	bool Initialise(unsigned short _usPortNumber);
+
+	SOCKET GetSocketHandle();
+
+	void SetRemotePort(unsigned short _usRemotePort);
+	void SetRemoteAddress(unsigned long _ulRemoteAddress);
+
+	int EnableBroadcast();
+	int DisableBroadcast();
+
+private:
+	std::string GetLocalAddress();
+	
+	SOCKET m_hSocket;						//The local socket
+	sockaddr_in m_SocketAddress;			//Socket address containing the local address and port of the socket
+
+	unsigned long m_ulRemoteIPAddress;		//The server address and port
+	unsigned short m_usRemotePort;
+};
